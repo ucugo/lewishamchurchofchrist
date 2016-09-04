c0ee6dc983d1fc0b5954b3227fa691edf4f1e438
chigozie <ucugo@yahoo.co.uk>
2016-09-04 23:26:19 +0100

    Added initial project structure

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+var churchOfChristApp = angular.module('app', []);
+
+churchOfChristApp.controller('appController', function($scope){
+    $scope.message = 'Everyone come and see how good I look!';
+});
