73f0149b1c70a9aaa6ed2341fc229a3fee577e3e
chigozie <ucugo@yahoo.co.uk>
2017-06-03 12:42:44 +0100

    created a menu template and moved menu nav from index file

--- js/churchAppController.js
+++ js/churchAppController.js
@@ -1,5 +1,5 @@
 churchOfChristApp.controller('appController', ["$scope", "$location", function($scope, $location){
-    $scope.$location = $location;
+    $scope.url = $location;
     $scope.currentYear = new Date().getFullYear()
     $scope.message = 'Everyone come and see how good I look!';
     $scope.myInterval = 2000;
