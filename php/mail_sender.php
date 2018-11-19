7cb116dc6a7d6121e445dfc1f6f4bf7566cab794
chigozie <ucugo@yahoo.co.uk>
2018-11-19 10:26:02 +0000

    Fixed email contact bug

--- php/mail_sender.php
+++ php/mail_sender.php
@@ -5,8 +5,12 @@ $name = $_REQUEST['name'];
 $subject = 'Contact form message';
 $message = "From: ".$from." Senders email: ".$from." Message: ".$_REQUEST['message'];
 
+$headers = 'From: webmaster@lewishamchurchofchrist.co.uk' . "\r\n" .
+    'Reply-To: webmaster@lewishamchurchofchrist.co.uk' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
 print($message);
-mail($to, $subject, $message, "From:" . $from);
+mail($to, $subject, $message, $headers);
 header("Location: ../index.html#!/contact");
     echo "<script type='text/javascript'>'Message sent'</script>"
 ?>
