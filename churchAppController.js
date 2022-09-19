15d05e3b74d13f4adea466b414c59becaa0d8ba2
chigozieuchegbu <ucugo1@gmail.com>
2022-09-19 10:09:02 +0100

    Updated address

---
+++ churchAppController.js
@@ -0,0 +1,91 @@
+churchOfChristApp.controller('appController', ["$scope", "$location", "WeeklyMessageDao", function($scope, $location, WeeklyMessageDao){
+    $scope.url = $location;
+    $scope.currentYear = new Date().getFullYear()
+    $scope.myInterval = 2000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+
+    $scope.status
+
+    var slides = $scope.slides = [];
+    $scope.messages
+    var currIndex = 0;
+
+
+    $scope.addSlide = function() {
+        slides.push({
+            image: 'img/church/rsz_img_1920.png',
+            text: [''][slides.length % 4],
+            id: currIndex++,
+            title: ''
+        }, {
+            image: 'img/church/rsz_img_1924.png',
+            text: ['',][slides.length % 4],
+            id: currIndex++,
+            title: ''
+        }, {
+            image: 'img/church/rsz_img_1071.png',
+            text: ['',][slides.length % 4],
+            id: currIndex++,
+            title: ''
+        });
+    };
+    $scope.addSlide();
+
+    $scope.sermons = [{
+        "title": "Admitting your sins and confessing to God:",
+        "audio_link_mp3": "http://lewishamchurchofchrist.co.uk/audio/sunday_message_2017_07_23.mp3",
+        "audio_link_ogg": "http://lewishamchurchofchrist.co.uk/audio/sunday_message_2017_07_23.mp3",
+        "preacher": "Br Johnson",
+        "date": "23-07-2017"
+    },
+        {
+            "title": "Called for a purpose:",
+            "audio_link_mp3": "http://lewishamchurchofchrist.co.uk/audio/Bro_sidney_02_sept_2017.mp3",
+            "audio_link_ogg": "http://lewishamchurchofchrist.co.uk/audio/Bro_sidney_02_sept_2017.mp3",
+            "preacher": "Br S Lewis",
+            "date": "03-09-2017"
+        }]
+
+
+
+    WeeklyMessageDao.async().then(function(d) {
+            $scope.messages = d;
+        });
+
+    $scope.randomize = function() {
+        var indexes = generateIndexesArray();
+        assignNewIndexesToSlides(indexes);
+    };
+
+    // Randomize logic below
+
+    function assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = slides.length; i < l; i++) {
+            slides[i].id = indexes.pop();
+        }
+    }
+
+    function generateIndexesArray() {
+        var indexes = [];
+        for (var i = 0; i < currIndex; ++i) {
+            indexes[i] = i;
+        }
+        return shuffle(indexes);
+    }
+
+    function shuffle(array) {
+        var tmp, current, top = array.length;
+
+        if (top) {
+            while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+            }
+        }
+
+        return array;
+    }
+}]);
--- php/mail_sender.php
+++ php/mail_sender.php
@@ -1,13 +1,18 @@
 <?php
-$to      = 'ucugo1@gmail.com';
-$from = $_REQUEST['email-address'];
-$name = $_REQUEST['name'];
-$subject = 'Contact form message';
-$message = "From: ".$from." Senders email: ".$from." Message: ".$_REQUEST['message'];
+$to        = 'ucugo1@gmail.com';
+$from      = $_REQUEST['email-address'];
+$name      = $_REQUEST['name'];
+$subject   = 'Church Contact form message';
+$telephone = $_REQUEST['telephone'];
+
+$message =  "Name: "         . $name      . "\t\n\n\n"
+            . "From: "       . $from      . "\r\n\n\n"
+            . "Telephone:  " . $telephone . "\r\n\n\n"
+            . "Message: "    . $_REQUEST['message'];
 
 $headers = 'From: webmaster@lewishamchurchofchrist.co.uk' . "\r\n" .
-    'Reply-To: webmaster@lewishamchurchofchrist.co.uk' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+           'Reply-To: webmaster@lewishamchurchofchrist.co.uk' . "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
 
 print($message);
 mail($to, $subject, $message, $headers);
